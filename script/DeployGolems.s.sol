40dc4baa141f7a64ee6137f6ad6d4bd8adc714bc
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-08 14:17:08 -0500

    basic file structure

---
+++ script/DeployGolems.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Script } from 'forge-std/Script.sol';
+import { Golems } from 'src/Golems.sol';
+
+contract DeployGolems is Script {}
