4c477ac43f43d912b0b06af5ced975ae43cc742d
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-08 16:30:51 -0500

    fix interactions mint golems script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.8.20;
 
 import { Script } from 'forge-std/Script.sol';
 import { Golems } from 'src/Golems.sol';
+import { DevOpsTools } from 'foundry-devops/src/DevOpsTools.sol';
 
 contract MintGolemsNFT is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment('Golems', block.chainid);
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+
     function mintNFTOnContract(address contractAddress) public {
         vm.startBroadcast();
         Golems(contractAddress).mintNFT();
