14b942020bd5335ced8be1e3f19f42225a10f568
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-09 11:08:09 -0500

    add further functionality and tests

--- src/Golems.sol
+++ src/Golems.sol
@@ -5,15 +5,63 @@ import { ERC1155 } from '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import { console } from 'forge-std/console.sol';
 
 contract Golems is ERC1155 {
+    error Golems__WhiteListPassesSoldOut();
+    error Golems__MinimumDonationNotMet();
+    error Golems__RequiresWhiteListPass();
+    error Golems__InsufficientPeanutBalance();
+
+    //********Token IDs *********//
+
+    // Used as tokenIds 0, 1, 2
     enum Starters {
         STICK,
         EMBER,
         DROPLET
     }
 
-    constructor() ERC1155('ipfs://QmURfC4rXTdhnQx8F8Ghq3jWHJbobrdkdevv5HcTyC2uts/{id}.json') {}
+    uint256 public constant PEANUTS = 100e10; // Starting tokenId, not the quantity
+    uint256 public constant WHITE_LIST_PASS = 101e10;
 
-    function userMintStarter(Starters starter) public {
+    //*******State Variables ********//
+    address owner;
+    uint256 public constant MINIMUM_WHITE_LIST_DONATION = 0.05 ether;
+    address[] public whiteListers;
+
+    constructor(address _owner) ERC1155('ipfs://QmURfC4rXTdhnQx8F8Ghq3jWHJbobrdkdevv5HcTyC2uts/{id}.json') {
+        owner = _owner;
+    }
+
+    function joinWhiteList() public payable {
+        if (whiteListers.length == 100) {
+            revert Golems__WhiteListPassesSoldOut();
+        }
+        if (msg.value < MINIMUM_WHITE_LIST_DONATION) {
+            revert Golems__MinimumDonationNotMet();
+        }
+        whiteListers.push(msg.sender);
+        _mint(msg.sender, WHITE_LIST_PASS, 1, '');
+    }
+
+    function claimWhiteListGift() public {
+        if (balanceOf(msg.sender, WHITE_LIST_PASS) == 0) {
+            revert Golems__RequiresWhiteListPass();
+        }
+        _mint(msg.sender, PEANUTS, 1000, '');
+    }
+
+    function mintStarter(Starters starter) public {
+        if (balanceOf(msg.sender, PEANUTS) < 10) {
+            revert Golems__InsufficientPeanutBalance();
+        }
+        safeTransferFrom(msg.sender, owner, PEANUTS, 10, '');
         _mint(msg.sender, uint256(starter), 1, '');
+    }
+
+    function evolveNFT(uint256 tokenId) public {
+        if (balanceOf(msg.sender, PEANUTS) < 50) {
+            revert Golems__InsufficientPeanutBalance();
+        }
+        _burn(msg.sender, tokenId, 1);
+        _mint(msg.sender, (tokenId + 3), 1, '');
     }
 }
--- test/GolemsTest.t.sol
+++ test/GolemsTest.t.sol
@@ -7,15 +7,85 @@ import { Golems } from 'src/Golems.sol';
 contract GolemsTest is Test {
     Golems golems;
 
+    address FOUNDRY_DEFAULT_CALLER = address(uint160(uint256(keccak256('foundry default caller'))));
     address USER = makeAddr('user');
+    uint256 public constant USER_INITIAL_BALANCE = 1 ether;
+    uint256 public constant MINIMUM_DONATION = 0.05 ether;
 
     function setUp() public {
-        golems = new Golems();
+        golems = new Golems(FOUNDRY_DEFAULT_CALLER);
+        vm.deal(USER, USER_INITIAL_BALANCE);
     }
 
-    function testURI() public {
+    modifier joinWhiteList() {
         vm.prank(USER);
-        golems.userMintStarter(Golems.Starters.STICK);
-        assert(golems.balanceOf(USER, 0) == 1);
+        golems.joinWhiteList{ value: MINIMUM_DONATION }();
+        _;
     }
+
+    /*//////////////////////////////////////////////////////////////
+                            JOIN WHITE LIST
+    //////////////////////////////////////////////////////////////*/
+
+    function testJoinWhiteListRevertsIfSoldOut() public {
+        uint256 whiteListSupply = 100;
+
+        for (uint256 i = 1; i <= whiteListSupply; i++) {
+            address newUser = address(uint160(i));
+            hoax(newUser, 1 ether);
+            golems.joinWhiteList{ value: MINIMUM_DONATION }();
+        }
+
+        vm.prank(USER);
+        vm.expectRevert(Golems.Golems__WhiteListPassesSoldOut.selector);
+        golems.joinWhiteList{ value: MINIMUM_DONATION }();
+    }
+
+    function testJoinWhiteListRevertsIfMinimumDonationNotMet() public {
+        vm.prank(USER);
+        vm.expectRevert(Golems.Golems__MinimumDonationNotMet.selector);
+        golems.joinWhiteList();
+    }
+
+    function testJoinWhiteListProperlyTransfersToDonater() public {
+        vm.prank(USER);
+        golems.joinWhiteList{ value: MINIMUM_DONATION }();
+        assert(golems.balanceOf(USER, golems.WHITE_LIST_PASS()) == 1);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         CLAIM WHITE LIST GIFT
+    //////////////////////////////////////////////////////////////*/
+
+    function testClaimWhiteListGiftRevertsIfUserDoesNotHaveAPass() public {
+        vm.prank(USER);
+        vm.expectRevert(Golems.Golems__RequiresWhiteListPass.selector);
+        golems.claimWhiteListGift();
+    }
+
+    function testClaimWhiteListGiftProperlyTransfersPeanutsToUser() public joinWhiteList {
+        vm.prank(USER);
+        golems.claimWhiteListGift();
+        assert(golems.balanceOf(USER, golems.PEANUTS()) == 1000);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              MINT STARTER
+    //////////////////////////////////////////////////////////////*/
+
+    function testMintStarterRevertsIfUserDoesNotHaveEnoughPeanutes() public {}
+
+    function testMintStarterTransfersPeanutsFromUserToContract() public {}
+
+    function testMintStarterProperlyMintsAnNFTForTheUser() public {}
+
+    /*//////////////////////////////////////////////////////////////
+                               EVOLVE NFT
+    //////////////////////////////////////////////////////////////*/
+
+    function testEvolveNFTRevertsIfUserDoesNotHaveEnoughPeanutes() public {}
+
+    function testEvolveNFTBurnsProperNFTFromUsersBalance() public {}
+
+    function testEvolveNFTProperlyMintsAnEvolvedNFTForTheUser() public {}
 }
